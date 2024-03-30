7405292d1a33fc298603cab855a86b517b47c03b
TamNhien <canhthien91@gmail.com>
2024-03-30 21:15:04 +0700

    Add project files.

---
+++ P_Sl_Nglieu_Max_MuaDuoc.py
@@ -0,0 +1,18 @@
+﻿def max_so_luong_nguyen_lieu(U, V, A, B):
+    max_so_luong = 0
+    
+    for a in A:
+        for b in B:
+            max_so_luong = max(max_so_luong, min(U / a, V / b))
+    
+    return max_so_luong
+
+U = float(input("Nhập số tiền U (USD): "))
+V = float(input("Nhập số tiền V (Euro): "))
+A = list(map(float, input("Nhập list giá bán Ai (USD/kg): ").split()))
+B = list(map(float, input("Nhập list giá bán Bi (Euro/kg): ").split()))
+
+result = max_so_luong_nguyen_lieu(U, V, A, B)
+print("Số lượng nguyên liệu mua được là: {:.2f} kg".format(result))
+
+
